12e7453ddcd38e2f91073ba287c58942d0625c01
faresMshams <faresghalwash2022@gmail.com>
2024-06-29 04:47:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const nightModeToggle = document.getElementById("night-mode-toggle");
     const contactLink = document.querySelector('a[href="#contact"]');
 
-    // Generate random percentages for skills
+    // Define skill percentages
+    const skillPercentages = {
+        "Python": "65%",
+        "Java": "32%",
+        "HTML": "75%",
+        "CSS": "61%",
+        "PHP": "30%",
+        "Visual Basic": "45%",
+        "Linux User": "40%",
+        "Reverse Engineering": "7%"
+    };
+
+    // Set percentages for skills
     percentages.forEach(span => {
-        span.textContent = `${Math.floor(Math.random() * 101)}%`;
+        const skillName = span.parentNode.textContent.trim().split(' ')[0];
+        span.textContent = skillPercentages[skillName] || "0%";
     });
 
     // Toggle night mode
